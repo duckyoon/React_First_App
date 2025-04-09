c1157fcc0c88c41532ad7d3b744e094909570b34
duckyoon <dkdlel616@gmail.com>
2025-04-09 22:20:22 +0900

    example: 250409_리액트 이밴트 예제

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -8,6 +8,7 @@ import ColorList from './ColorList';
 import Slots from './Slots';
 import ShoppingList from './ShoppingList';
 import PropertyList from './PropertyList';
+import Clicker from './Clicker';
 // const data = [
 //   {id: 1, item:"eggs", quantity: 12, completed: false},
 //   {id: 2, item:"milk", quantity: 5, completed: true},
@@ -15,19 +16,21 @@ import PropertyList from './PropertyList';
 //   {id: 4, item:"breed", quantity: 44, completed: true},
 // ]
 
-const properties = [
-  { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
-  { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
-  { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
-  { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
-  { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
-  { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
-];
+// const properties = [
+//   { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
+//   { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
+//   { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
+//   { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
+//   { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
+//   { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
+// ];
 
 function App() {
   return (
     <div>
-      <PropertyList items={properties}/>      
+      <Clicker message="Hi!!!" buttonText="Please click me"/>
+      <Clicker message="Please Stop Clicking me!" buttonText="Do not click me"/>
+      {/* <PropertyList items={properties}/>       */}
       {/* <ShoppingList items={data}/> */}
       {/* <Greeter person="Bill" from="jj"/>
       <Greeter />
